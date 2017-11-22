04c154d6718da019e20363794510d6b3fc4a7071
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2017-11-22 02:54:34 -0500

    testa

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 //index.js
 //alert("Alert works");
-document.getElementById('JQTest').style.background-color = "black";
+document.getElementById("JQTest").style.background-color = "black";
 
 
 //window.onload = function () { alert("It's loaded!") }
