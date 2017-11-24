ba589a58024f9112e1eecd294c1a8f9c7b6f9799
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2017-11-24 15:31:06 -0500

    new page format

---
+++ javascript/splash.js
@@ -0,0 +1,10 @@
+//splash.js
+//alert("Alert works");
+
+$("#myButton").click(jqueryTest);
+function jqueryTest() {
+	$JQ = $("#JQTest h1");
+	$JQ.html("JQuery works!")
+}
+
+//document.getElementById('JQTest').style.background-color = "black";
