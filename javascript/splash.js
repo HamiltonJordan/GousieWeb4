1cbe5159dd7e4bd2a079f825ae07d10288ab91d5
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2017-11-28 11:46:50 -0500

    adding new login button

--- javascript/splash.js
+++ javascript/splash.js
@@ -12,9 +12,18 @@ $(".bubble").hover(
 		$(this).animate({
 			width: '100%'
 		}); 
-	});
+	}
+);
 $(".bubble").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
+	window.location = $(this).find("a").attr("href"); 
+	return false;
+});
+
+$("#login").click(function() {
+	$("#cover").show(1);
+	$("#cover").animate({
+		opacity: '0.8'
+	})
+	//$("#cover").show(2500);
 });
 //document.getElementById('JQTest').style.background-color = "black";
