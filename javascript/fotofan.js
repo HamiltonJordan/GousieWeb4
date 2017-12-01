03cde8e00ac6a2ce8050c25d5832d1fa7cfbb55f
JordanHelpsAlot <hamilton_jordan@wheatoncollege.edu>
2017-11-30 19:30:08 -0500

    new fotofan work

---
+++ javascript/fotofan.js
@@ -0,0 +1,60 @@
+//splash.js
+//alert("Alert works");
+
+$(".bubble").hover(
+	function(){
+		$(this).animate({
+			color: '#00ff00',
+			width: '110%'
+		}); 
+	},  
+	function(){
+		$(this).animate({
+			width: '100%'
+		}); 
+	}
+);
+$(".bubble").click(function() {
+	window.location = $(this).find("a").attr("href"); 
+	return false;
+});
+
+$("#login_button").click(function() {
+	$("#cover").show(1);
+	$("#cover").animate({
+		opacity: '0.8'
+	})
+	$("#login_window").show(700);
+
+	//$("#cover").show(2500);
+});
+
+$("#close").click(function() {
+	$("#login_window").hide();
+	$("#cover").hide();
+});
+
+
+/*
+$.ajax({
+    type: "GET",
+    url: 'php/login.php',
+    data: {user: 'jordan'},
+    success: function(data){
+        alert(data);
+    }
+});
+*/
+
+	/*
+	// Make an Ajax request to login.php with the entered ZIP code
+	var notNeeded;
+	$.get("http://cs.wheatoncollege.edu/~jhamilton/login.php?user="+user, notNeeded, function (response) {
+		alert(response);
+	})
+	*/
+
+
+
+//document.getElementById('JQTest').style.background-color = "black";
+//jshint multistr: true
