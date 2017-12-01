6a1a95bb9e6b962afb62e1b2076258d332ef92df
Jacob Kydd <kydd_jacob@wheatoncollege.edu>
2017-11-30 20:43:42 -0500

    added php login check

--- javascript/splash.js
+++ javascript/splash.js
@@ -26,4 +26,29 @@ $("#login").click(function() {
 	})
 	//$("#cover").show(2500);
 });
+
+$("login_button").click(function() {
+   // Get the ZIP code from the text field
+	var user_name = $("#user_name").val();
+	var password = $("#password").val();
+
+   //alert (zip);
+   // Make an Ajax request to weather.php with the entered ZIP code
+   var notNeeded;
+   $.get("weather2.php?zip="+zip, notNeeded, function (response) {
+      var myObj = JSON.parse (response);
+      //alert (myObj.success);
+      var html = "";
+      if (myObj.success) {
+         html += "<h3>Forecast</h3><ol>"
+         myObj.forecast.forEach (function (day) {
+            html += "<li>" + day.desc + ": High is " + day.high + ", low is " + day.low + "</li>";
+         });
+
+      } else {
+         html += "<h3>Zip code not found</h3>";
+      }
+      $("#forecast").html(html);
+   });
+});
 //document.getElementById('JQTest').style.background-color = "black";
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$zip = $_GET["credentials"];
+
+class User {
+    public $success = false;
+    public $gallery = "";
+}
+
+if ($username === "Joe" and $password === "joe123") {
+    $user1 = new User();
+    $user1->success = true;
+    $user1->gallery = gousie;
+}
+
+if ($username === "Jane" and $password === "jane123") {
+    $user1 = new User();
+    $user1->success = true;
+    $user1->gallery = jane;
+}
+
+?>
