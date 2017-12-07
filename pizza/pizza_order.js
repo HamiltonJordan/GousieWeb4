9db1314da4bd477e2718277c97af14854e7eabe6
Jacob Kydd <kydd_jacob@wheatoncollege.edu>
2017-12-07 16:16:00 -0500

    added images

---
+++ pizza/pizza_order.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#cheese").click(function() {
+       alert("cheese"); 
+    });
+    $("#pepperoni").click(function() {
+       alert("cheese"); 
+    });
+    $("#meat").click(function() {
+       alert("cheese"); 
+    });
+});
